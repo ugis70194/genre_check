4a7976097d07312c965f9beb1f2230321a243a4d
ugis70194 <ugis70194@yahoo.co.jp>
2023-07-15 14:35:22 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const { Toolkit } = require('actions-toolkit');
+const jsonc = require('jsonc');
+const fs = require('fs');
+
+const appRoot = `${process.env.GITHUB_WORKSPACE}`;
+const targetDir = process.env.INPUT_TARGET_DIRECTORY;
+const workingDir = `${appRoot}/${targetDir}`;
+const genres = ["Manga", "Novel", "Illust", "CD", "Goods", "PhotoAlbum","Anthology", "Contribution", "Consideration"];
+
+Toolkit.run(async tools => {
+  console.log(workingDir);
+  try {
+    const works = fs.readdirSync(workingDir);
+    let hasUnknownGenre = 0;
+
+    for(const work of works){
+      // detail.jsoncの読み込み
+      const readTargetDir = `${workingDir}/${work}`; 
+      const detailPath = `${readTargetDir}/${process.env.INPUT_TARGET_JSONC}`;
+      console.log(readTargetDir);
+      const detail = jsonc.parse(fs.readFileSync(detailPath, "utf-8"));
+      // genreが存在していなければエラーを吐く
+      if(genres.find(genre => genre === detail.genre) === undefined){
+        hasUnknownGenre = 1;
+        console.error(`不明なジャンル ${detail.genre} があります。コピー&ペーストで記入し直してみてください`);
+      }
+    }
+    if(hasUnknownGenre){
+      tools.exit.failure("has unknown genre");
+    }
+  } catch (e) {
+    tools.log.fatal(e);
+    tools.exit.failure('Failed');
+  }
+})
